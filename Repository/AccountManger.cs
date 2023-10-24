296cf1b41654e7ed7e818b5a8c5bb752bae21601
99khalid <khalidhassan912999@gmail.com>
2023-10-24 08:07:40 +0300

    task day 8

---
+++ Repository/AccountManger.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel;
+
+namespace Repository
+{
+    public class AccountManger:MainManager<User>
+    {
+        UserManager<User> userManager;
+        SignInManager<User> signInManager;
+        public AccountManger(MyDBContext myDBContext,
+            UserManager<User> _userManager, 
+            SignInManager<User> _signInManager
+         ) 
+            : base(myDBContext) {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        public async Task<IdentityResult> SignUp(UserSignUpViewModel Viewmodel)
+        {
+            var model = Viewmodel.ToModel();
+            var result = await userManager.CreateAsync(model,Viewmodel.Password);
+            if (result.Succeeded)
+            {
+                result = await userManager.AddToRoleAsync(model, Viewmodel.Role);
+            }
+            return result;
+
+        }
+        public async Task<SignInResult> SignIn (UserSignInViewModel viewModel)
+        {
+           return await signInManager.PasswordSignInAsync(viewModel.UserName, 
+                  viewModel.Password,viewModel.RememberMe,true);
+        }
+        public async void SignOut() {
+            await signInManager.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> ChangePassword(UserChangePasswordViewModel viewModel) {
+            var user = await userManager.FindByIdAsync(viewModel.Id); 
+            if (user != null) {
+               return await userManager.ChangePasswordAsync(user, viewModel.CurrentPassword, viewModel.NewPassword);
+            }
+            return IdentityResult.Failed(new IdentityError()
+            {
+                Description="User Not Found"
+            });
+        }
+
+        public async Task<string> GetForgotPasswordCode(string Email) {
+            var user = await userManager.FindByEmailAsync(Email);
+            if(user != null)
+            {
+                var code = await userManager.GeneratePasswordResetTokenAsync(user);
+                return code;
+            }
+            return string.Empty;
+        }
+        public async Task<IdentityResult> ForgotPassword(UserForgotPasswordViewModel viewModel)
+        {
+            var user = await userManager.FindByEmailAsync(viewModel.Email);
+            if (user != null)
+            {
+                return await userManager.ResetPasswordAsync(user,viewModel.Code,viewModel.NewPassword);
+            }
+            return IdentityResult.Failed(new IdentityError()
+            {
+                Description = "User Not Found"
+            });
+        }
+        public async Task<IdentityResult> AssignRolesToUser(string UserId,List<string> roles)
+        {
+            var user= await userManager.FindByIdAsync(UserId);
+            if (user != null)
+            {
+                return await userManager.AddToRolesAsync(user, roles);
+            }
+            return new IdentityResult();
+        } 
+
+    }
+}
---
+++ Web Application/Controllers/AccountController.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Repository;
+using System.Net.Mail;
+using System.Net;
+using System.Security.Claims;
+using ViewModel;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Web_Application.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountManger accManger;
+        RoleManager roleManager;
+        public AccountController(AccountManger _accManger,RoleManager _roleManager)
+        {
+            accManger = _accManger;
+            roleManager = _roleManager;
+        }
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            ViewData["list"] = RoleList();
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserSignUpViewModel viewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityResult result= await  accManger.SignUp(viewModel);
+                if (result.Succeeded)
+                {
+                     return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                    ViewData["list"] = RoleList();
+                    return View();
+                }
+            }
+            ViewData["list"] = RoleList();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn(string ReturnUrl = "/")
+        {
+            //var veiwModel = new UserSignInViewModel() { ReturnUrl= ReturnUrl };
+            //return View(veiwModel);
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel viewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await accManger.SignIn(viewModel);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Product");
+                }
+                else if (result.IsLockedOut)
+                {
+                    ModelState.AddModelError("", "Your Account is Under Reivew");
+                    return View();
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invaild User Name Or Password");
+                    return View();
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult SignOut()
+        {
+             accManger.SignOut();
+            return RedirectToAction("SignIn");
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult ChangePassword()
+        {
+            
+            ViewBag.Success = false;
+            return View();
+        }
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> ChangePassword(UserChangePasswordViewModel viewModel)
+        {
+            viewModel.Id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (ModelState.IsValid)
+            {
+                var result = await accManger.ChangePassword(viewModel);
+                if (result.Succeeded)
+                {
+                     ViewBag.Success = true;
+                }
+                return View();
+            }
+            ViewBag.Success = false;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            ViewBag.Success = false;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(string Email)
+        {
+            if(!string.IsNullOrEmpty(Email))
+            {
+               string code= await accManger.GetForgotPasswordCode(Email);
+                if (string.IsNullOrEmpty(code))
+                {
+                    ViewBag.Success = false;
+                }
+                else
+                {
+                    //Send Mail With Code
+                    var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+                    {
+                        Credentials = new NetworkCredential("6ecff26c8406b7", "83304a61e8f702"),
+                        EnableSsl = true
+                    };
+                    client.Send("from@example.com", Email, "Forget Password Verification", $"Your Code is {code}");
+                    ViewBag.Success = true;
+                }
+                return View();
+            }
+            ViewBag.Success = false;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ForgotPasswordVerification()
+        {
+            ViewBag.Success = false;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgotPasswordVerification(UserForgotPasswordViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await accManger.ForgotPassword(viewModel);
+                if (!result.Succeeded)
+                {
+                    ViewBag.Success = false;
+                }
+                else
+                {
+                    ViewBag.Success = true;
+                }
+                return View();
+            }
+            ViewBag.Success = false;
+            return View();
+        }
+        private List<SelectListItem> RoleList()
+        {
+            return roleManager.GetList().Select(r => new SelectListItem()
+            {
+                Value = r.Name,
+                Text  = r.Name
+            }).ToList();
+        }
+    }
+}
